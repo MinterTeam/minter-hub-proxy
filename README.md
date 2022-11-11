0d793c3911f82916527866ec25702e12ed8f2455
Daniil Lashin <danil-lashin@yandex.ru>
2022-11-11 10:42:16 +0300

    Prettify

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+# Minter Hub Proxy contract
+
+## Description
+Minter Hub Proxy is a smart contract in EVM networks that allows you to perform any operations on blockchains and return received coins to Minter DEX through Minter Hub.
+
+The main function of Minter Hub Proxy is on-chain calculation of the amount of coins received as a result of a tx and sending them to Minter Hub in one transaction.
+
+## How it works
+```
+  contractToCall = "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd";
+  contractData = "0xd0e30db0";
+  tokenToSpend = "0x0000000000000000000000000000000000000001";
+  maxAmountToSpend = "1";
+  tokenToReceive = "0x0000000000000000000000000000000000000002";
+  refundTo = "0x0000000000000000000000000000000000000000";
+  hubDestination = "0x0000000000000000000000003f92f12268f24ee429954eec58431c1842c44535";
+  hubDestinationChain = "0x6d696e7465720000000000000000000000000000000000000000000000000000";
+  
+  // Call contractToCall with contractData and send tokenToReceive to Minter Hub
+  proxy.callAndTransferToChain(contractToCall, contractData, tokenToSpend, maxAmountToSpend, tokenToReceive, refundTo, destinationChain, destination, 0);
+```
+
+*Note*: you also can send an eth value to contract. In this case `tokenToSpend` and `maxAmountToSpend` must be equal to `0x0000000000000000000000000000000000000000` and `0`.
+
 ## Deploy testnet
 
 ```shell
--- contracts/MinterHubProxy.sol
+++ contracts/MinterHubProxy.sol
@@ -21,20 +21,39 @@ contract MinterHubProxy {
         IERC20 tokenFrom,
         uint256 tokenFromAmount,
         IERC20 tokenTo,
+        address refundTo,
         bytes32 destinationChain,
         bytes32 destination,
         uint256 fee
     ) public payable {
         if (msg.value == 0) {
+            // deposit "tokenFrom" to this contract
             tokenFrom.transferFrom(msg.sender, address(this), tokenFromAmount);
+
+            // approve "to" contract to spend "tokenFrom"
             tokenFrom.approve(to, tokenFromAmount);
+        } else {
+            require(tokenFrom == 0x0000000000000000000000000000000000000000 && tokenFromAmount == 0, "MinterHubProxy: set tokenFrom and tokenFromAmount to 0 when passing a value");
         }
 
+        // call "to" contract and calculate the difference of balance of "tokenTo" before and after
         uint256 balanceBefore = tokenTo.balanceOf(address(this));
         Address.functionCallWithValue(to, data, msg.value);
         uint256 toDeposit = tokenTo.balanceOf(address(this)) - balanceBefore;
 
+        // approve "tokenTo" to hub and call "transferToChain"
         tokenTo.approve(address(hub), toDeposit);
         hub.transferToChain(address(tokenTo), destinationChain, destination, toDeposit, fee);
+
+        // refund any remaining tokens
+        if (msg.value == 0) {
+            if (tokenFrom.balanceOf(address(this)) > 0) {
+                tokenFrom.transfer(refundTo, tokenFrom.balanceOf(address(this)));
+            }
+        } else {
+            if (address(this).balance > 0) {
+                Address.sendValue(payable(refundTo), address(this).balance);
+            }
+        }
     }
 }
